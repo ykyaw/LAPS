6b42626b917bf4c0dee7a60261e41b31539112c3
WUYUDING <e0533355@u.nus.edu>
2020-06-27 14:39:13 +0800

    create la,overtime and public holiday repository

---
+++ src/main/java/com/team1/iss/trial/repo/LARepository.java
@@ -0,0 +1,9 @@
+package com.team1.iss.trial.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.team1.iss.trial.domain.LA;
+
+public interface LARepository  extends JpaRepository<LA, Integer>{
+
+}
