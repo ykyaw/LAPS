3a20d46957492167a74dd6b65303ab2ac3b34273
TK <e0533379@u.nus.edu>
2020-06-27 11:17:00 +0800

    edited security config and common. html. Removed the ROLE_ functionality of spring security, now we no longer need to work with ROLE_

--- src/main/java/com/team1/iss/trial/config/SecurityConfig.java
+++ src/main/java/com/team1/iss/trial/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -26,11 +27,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 //		.usersByUsernameQuery("select username, password, enabled from user where username = ?")
 //		.authoritiesByUsernameQuery("select username, authorities from user where username = ?");
 //	}
+	@Bean
+	GrantedAuthorityDefaults grantedAuthorityDefaults() {
+	    return new GrantedAuthorityDefaults(""); // Remove the ROLE_ prefix
+	}
+	
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
 		auth.jdbcAuthentication().dataSource(dataSource)
-		.usersByUsernameQuery("SELECT t1.name, t1.password, t1.enabled FROM user AS t1 where t1.name = ?")
+		.usersByUsernameQuery("select name, password, enabled from user where name = ?")
 		.authoritiesByUsernameQuery("select name, user_type from user where name = ?");
 	}
 	
@@ -38,9 +44,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure (HttpSecurity http) throws Exception{
 		http.csrf().disable()
 		.authorizeRequests() //This means we need to authorise all requests
-		.antMatchers("/manager/**").hasRole(CommConstants.UserType.MANAGER) //This means for the manager API, allow only those with manager role. ** means anything after /manager/ also controlled
-		.antMatchers("/admin/**").hasRole(CommConstants.UserType.AMDIN) //This means for the admin API, allow only those with ADMIN role. ** means anything after /admin/ also controlled
-		.antMatchers("/employee/**").hasAnyRole(CommConstants.UserType.AMDIN,CommConstants.UserType.EMPLOYEE,CommConstants.UserType.MANAGER) // This means for users API, allow anyone with either ADMIN or USER role
+		.antMatchers("/manager/**").hasAuthority(CommConstants.UserType.MANAGER) //This means for the manager API, allow only those with manager role. ** means anything after /manager/ also controlled
+		.antMatchers("/admin/**").hasAuthority(CommConstants.UserType.AMDIN) //This means for the admin API, allow only those with ADMIN role. ** means anything after /admin/ also controlled
+		.antMatchers("/employee/**").hasAnyAuthority(CommConstants.UserType.AMDIN,CommConstants.UserType.EMPLOYEE,CommConstants.UserType.MANAGER) // This means for users API, allow anyone with either ADMIN or USER role
 		.antMatchers("/").permitAll() //This is the root, means at root level, permit everyone
 		.and().formLogin().loginPage("/login")
 		.and()
