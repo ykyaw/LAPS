4aa3b83e8f3d3336157b31f68fa3137220754c5c
jaynep <e0533374@u.nus.edu>
2020-06-28 10:56:59 +0800

    update on creating users and adding functions (update role, create)

--- src/main/java/com/team1/iss/trial/repo/UserRepository.java
+++ src/main/java/com/team1/iss/trial/repo/UserRepository.java
@@ -3,8 +3,12 @@ package com.team1.iss.trial.repo;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.team1.iss.trial.domain.User;
 
@@ -16,5 +20,12 @@ public interface UserRepository extends JpaRepository<User, Integer>{
 	
 //	@Query("Select u.username from User u")
 //	ArrayList<String> findAllUsernames();
+
+
+
+@Modifying 
+@Transactional
+@Query ("update User set user_type=:userType where uid=:uid")
+public void updateUserType(@Param("userType") String userType,@Param("uid") int uid);
 
 }
