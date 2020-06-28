52a024f226529832c8d666e5fb5ee9a6ee1d1c59
Johnnyqfc <zhangyuanchang0716@gmail.com>
2020-06-28 20:13:35 +0800

    add laDetails.html Create uncompleted methods of CRUD of LA under employee controller Create LA service interface and implementation

--- src/main/java/com/team1/iss/trial/controller/EmployeeController.java
+++ src/main/java/com/team1/iss/trial/controller/EmployeeController.java
@@ -1,17 +1,56 @@
 package com.team1.iss.trial.controller;
 
+import com.team1.iss.trial.domain.LA;
+import com.team1.iss.trial.services.impl.LaServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+@RestController
 @RequestMapping("/employee")
 public class EmployeeController {
-	
+	@Autowired
+	private LaServiceImpl laServiceImpl;
+
 	@RequestMapping("")
 	public String user() {
 		return ("employee/eHome");
 	}
-	
-	
+
+	//Get All LAs
+	@RequestMapping(value= "/las", method = RequestMethod.GET)
+	public String listAllLAs(Model model) {
+		model.addAttribute("LA", laServiceImpl.findAll());
+		return "las";
+
+	}
+
+	//Get single LA details by provide id
+	@RequestMapping(value= "/la/{uid}", method = RequestMethod.GET)
+	public String getLAById(Model model, @PathVariable int uid) {
+		model.addAttribute("LA",laServiceImpl.getLaById(uid));
+		return "laDetails";
+	}
+
+	//Create a new LA with full LA details info in Body
+	@RequestMapping(value="/la", method = RequestMethod.POST)
+	public void saveLA(@RequestBody LA la) {
+		laServiceImpl.saveLA(la);
+	}
+
+	//Update an existing LA with udpated Body, not sure how to input uid
+	@RequestMapping(value="/la/{uid}", method = RequestMethod.PUT)
+	public void updateLA(@RequestBody LA la, int uid) {
+		laServiceImpl.updateLA(la);
+	}
+
+	//Delete an existing LA
+	@RequestMapping(value="/la/{uid}", method = RequestMethod.DELETE)
+	public void deleteLA(int uid){
+		laServiceImpl.deleteLA(uid);
+	}
 
 }
--- src/main/java/com/team1/iss/trial/repo/LARepository.java
+++ src/main/java/com/team1/iss/trial/repo/LARepository.java
@@ -3,7 +3,39 @@ package com.team1.iss.trial.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.team1.iss.trial.domain.LA;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface LARepository  extends JpaRepository<LA, Integer>{
+import java.util.List;
+import java.util.Optional;
+
+/*
+ * Author: YC
+ * */
+public interface LARepository extends JpaRepository<LA, Integer>{
+
+    @Query("FROM LA WHERE uid = ?1")
+    public List<LA> findLADetailsByUid(int uid);
+
+    @Query("FROM LA WHERE owner_id = ?1")
+    public List<LA> findLADetailsByOwnerId(int ownerid);
+
+    @Query("FROM LA WHERE status = ?1")
+    public List<LA> findLADetailsByStatus(String status);
+
+    @Modifying
+    @Transactional  //Spring transactional
+    @Query("update LA set LA.uid = ?1 where uid=LA.uid")
+    public void updateLA(@Param("la") LA la);
+
+//    @Query("update LA set fromTime=:#{#la.fromTime},xxx=xxx where uid=:#{#la.uid}")
+//    public void updateLA1(@Param("la") LA la);
+
+//    @Query("update LA set LA.firstname = ?1, LA.lastname = ?2 where LA.uid = ?1")
+
+
+//    void setUserInfoById(String firstname, String lastname, Integer userId);
 
 }
