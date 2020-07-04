1981f26c49575ce185386ffda64532c3d561d7c3
TK <e0533379@u.nus.edu>
2020-07-04 16:28:58 +0800

    missed out 1 file from previous push

--- src/main/java/com/team1/iss/trial/Team1TrialApplication.java
+++ src/main/java/com/team1/iss/trial/Team1TrialApplication.java
@@ -3,14 +3,13 @@ package com.team1.iss.trial;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.team1.iss.trial.domain.Manager;
 
 @SpringBootApplication
 public class Team1TrialApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Team1TrialApplication.class, args);
-		Manager manager=new Manager();
+		
 	}
 
 }
